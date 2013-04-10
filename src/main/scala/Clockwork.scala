ecebe5909088fe6ab8818fbd7dba3f2fee9bd994
jasonnerothin <jasonnerothin@superiorbiomining.com>
2013-04-10 00:47:07 -0500

    documentation update

--- src/main/scala/Clockwork.scala
+++ src/main/scala/Clockwork.scala
@@ -6,13 +6,13 @@
 trait Clockwork {
 
   /** @param minutesSinceMidnight absolute number since midnight
-    * @return degrees of minute hand movement (since midnight)
+    * @return degrees of minute hand movement (since midnight, whole numbers only)
     */
   def degreesOf(minutesSinceMidnight: Int): Int =
     minutesSinceMidnight * 6
 
   /** @param degrees amount of minute hand movement
-    * @return equivalent value in minutes
+    * @return equivalent value in minutes, whole minutes only
     */
   def minutesOf(degrees: Int): Int = {
     val deg = degrees / 6.0
