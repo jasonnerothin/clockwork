af131be1d50c208ca957e88a9603a0432e50088d
jasonnerothin <jasonnerothin@superiorbiomining.com>
2013-04-09 23:04:22 -0500

    A test utility for data gen...

---
+++ src/test/scala/testutil/Generator.scala
@@ -0,0 +1,75 @@
+package testutil
+
+import util.Random
+import org.joda.time.DateTime
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jason
+ * Date: 10/31/12
+ * Time: 12:24 AM
+ */
+class Generator{
+
+  case class A(b : Boolean, i : Int, l : Long, ch: Char)
+
+  def someAs(): Seq[A] = {
+    someAs(10)
+  }
+
+  def someAs(len: Int): Seq[A] = 0 to len map( xml =>
+    A(  Random.nextBoolean(),
+        Random.nextInt(),
+        Random.nextLong(),
+        (Random.nextInt(26) + offset(Random.nextBoolean())).toChar
+    )
+  )
+
+  private def offset(b: Boolean): Int = {
+    if ( b ) 65 else 97
+  }
+
+  /**
+   * Random string, length 10
+   */
+  def randomStr: String = {
+    randomStr(10)
+  }
+
+  /**
+   * Arbitrarily length random string
+   * @param len desired length
+   * @return the string
+   */
+  def randomStr(len: Int): String = {
+    val buf = new StringBuilder
+    val as = someAs(len)
+    for { i <- 0 to len-1} buf.append(as(i).ch)
+    buf.toString()
+  }
+
+  /**
+   * @return random date, at least one second in the past
+   */
+  def dateInThePast(): DateTime = {
+    new DateTime()
+      .minusYears(Random.nextInt(10))
+      .minusMonths(Random.nextInt(12))
+      .minusHours(Random.nextInt(24))
+      .minusMinutes(Random.nextInt(60))
+      .minusSeconds(Random.nextInt(59) + 1)
+  }
+
+  /**
+   * @return a random date at least 15 seconds from now
+   */
+  def dateInTheFuture(): DateTime = {
+    new DateTime()
+      .plusYears(Random.nextInt(10))
+      .plusMonths(Random.nextInt(12))
+      .plusHours(Random.nextInt(24))
+      .plusMinutes(Random.nextInt(60))
+      .plusSeconds(Random.nextInt(45)+15)
+  }
+
+}
---
+++ src/test/scala/testutil/GeneratorTest.scala
@@ -0,0 +1,49 @@
+package testutil
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.joda.time.DateTime
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jason
+ * Date: 10/31/12
+ * Time: 8:38 PM
+ * Provides...
+ */
+@RunWith(classOf[JUnitRunner])
+class GeneratorTest extends FunSuite {
+
+  val testInstance = new Generator
+
+  test("randomStr is the right length"){
+    val actual = testInstance.randomStr
+
+    assert(actual.length === 10, "supposed to be 10 long")
+
+  }
+
+  test("randomStr is the right (arbitrary) length"){
+    val actual = testInstance.randomStr(56)
+
+    assert(actual.length === 56, "56 is just right")
+  }
+
+  test("random date in the past at least 1 second in the past"){
+    val now = new DateTime()
+    val actual = testInstance.dateInThePast()
+
+    assert( now.minusSeconds(1) isAfter actual )
+
+  }
+
+  test("date in the future is at least 15 seconds in the future"){
+    val now = new DateTime()
+    val actual = testInstance.dateInTheFuture()
+
+    assert(actual.minusSeconds(15) isAfter now)
+
+  }
+
+}
