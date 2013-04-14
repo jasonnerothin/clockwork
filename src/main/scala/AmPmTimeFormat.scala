c55277edf04962763040163bf8e650ea1dd80a3b
jasonnerothin <jasonnerothin@superiorbiomining.com>
2013-04-14 17:01:37 -0500

    adding doc

--- src/main/scala/AmPmTimeFormat.scala
+++ src/main/scala/AmPmTimeFormat.scala
@@ -11,13 +11,16 @@ class AmPmTimeFormat(time: String) {
 
   validate()
 
+ /** validate that the time string is in teh correct format
+   */
   def validate() {
     if (!format.matcher(time).matches()) {
       throw new IllegalArgumentException("Expected a time like '12:13 PM', but got '" + time + "' instead.")
     }
   }
 
-
+ /** @return number of minutes since midnight
+   */
   def minuteOfDay(): Int = {
     def beforeNoon() = time.contains("am") || time.contains("AM") || time.contains("Am") || time.contains("aM")
     def hourPart() = time.substring(0, time.indexOf(":")).trim
