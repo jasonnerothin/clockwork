e02e865cef9cd31f8ec944bedc80c8719b5904e5
jasonnerothin <jasonnerothin@superiorbiomining.com>
2013-04-14 21:57:06 -0500

    Updating README, making console "UI" a little easier to use.

--- README.md
+++ README.md
@@ -5,9 +5,27 @@ Written by: Jason Nerothin || http://jasonnerothin.com
 
 A project having to do with clocks and such...
 
+Prerequisites
+-------------
+1. SBT installed and ready to go.
+
+Description
+-----------
+
 This project provides a console application that calculates the required travel of an abstract clock's
 minute hand from one time to another.
 
+Ancillary details
+-----------------
+
 This app can be built using Scala's simple build tool (SBT) and tested and run with version 2.9.2 of scala
 on top of a bootstrapped 1.7 version of Oracle's OpenJDK. I expect it would work equally well with a 1.6
-JDK. All dependencies are available automagically in the build file.
+JDK.
+
+To Run
+------
+
+To run the application, open a command window in the project directory and type 'sbt'. Then, type
+'run-main Main' (no quotes) and hit enter. A number of other useful SBT tasks are available such as 'test',
+'compile', 'package', 'reload', etc...
+
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,11 +11,11 @@ object Main {
     */
   def main(args: Array[String]) {
 
-    print("START: ")
-    val t0 = makeAClock()
+    println("\nThis application calculates the amount of travel (in degrees) required to move the minute hand on an " +
+      "abstract clock from one time to another. You will need to provide a start and stop time.\n")
 
-    print("STOP: ")
-    val t1 = makeAClock()
+    val t0 = makeAClock("Provide start time: ")
+    val t1 = makeAClock("Provide stop time: ")
 
     val degreeTravel = new ClockSynchronizer(t0, t1).degreesBetween()
     println("The minute hand must travel " + degreeTravel + " degrees to get from " + t0 + " to " + t1 + ".")
@@ -23,14 +23,14 @@ object Main {
   }
 
   /** @return keep asking the user for input until s/he types in a valid time. */
-  def makeAClock(): Clock = {
-    println("Please enter a time with the following format: HH:mm AM|PM). Then hit enter.")
+  def makeAClock(str:String): Clock = {
+    println(str+"Please enter a time with the following format: HH:mm AM|PM. Then hit enter.")
     try {
       new Clock(new AmPmTimeFormat(readLine()).minuteOfDay())
     } catch {
       case e: IllegalArgumentException =>
         println(e.getMessage)
-        makeAClock()
+        makeAClock(str)
     }
   }
 
