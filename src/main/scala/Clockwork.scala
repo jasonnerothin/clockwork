f1cfd58911004edf9395f0b95b9c9bb65a6436aa
jasonnerothin <jasonnerothin@superiorbiomining.com>
2013-04-14 19:52:00 -0500

    cleanup

--- src/main/scala/Clockwork.scala
+++ src/main/scala/Clockwork.scala
@@ -5,7 +5,7 @@
   */
 trait Clockwork {
 
-  val MINUTES_PER_DAY = 24*60
+  val MINUTES_PER_DAY = 24 * 60
 
   /** @param minutesSinceMidnight absolute number since midnight
     * @return degrees of minute hand movement (since midnight, whole numbers only)
@@ -13,20 +13,19 @@ trait Clockwork {
   def degreesOf(minutesSinceMidnight: Int): Int =
     minutesSinceMidnight * 6
 
- /** @return the last minute of the model day
-   */
+  /** @return the last minute of the model day  */
   def lastMinuteOfDay(): Int = MINUTES_PER_DAY - 1
 
- /** Calculate the next minute on the model clock
-   * @param time some current position on the clock
-   * @return the next (forward) location on the clock after min
-   */
+  /** Calculate the next minute on the model clock
+    * @param time some current position on the clock
+    * @return the next (forward) location on the clock after min
+    */
   def tickTock(time: Int): Int = {
-   if( time >= MINUTES_PER_DAY )
+    if (time >= MINUTES_PER_DAY)
       throw new IllegalArgumentException("Minute value {" + time + "} is greater than the number of minutes in a day {" + MINUTES_PER_DAY + "}")
-   if( time < 0 )
-     throw new IllegalArgumentException("Minute value {" + time + "} is negative, which is not allowed.")
-    if( time != lastMinuteOfDay() ) time + 1
+    if (time < 0)
+      throw new IllegalArgumentException("Minute value {" + time + "} is negative, which is not allowed.")
+    if (time != lastMinuteOfDay()) time + 1
     else 0
   }
 
